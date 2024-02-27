fd40f75b8e7b08fb17b355891096321817a0445f
johnGavDev <143215466+johnGavDev@users.noreply.github.com>
2024-02-27 22:27:25 +0000

    v1

--- fetchRes.js
+++ fetchRes.js
@@ -1,8 +1,8 @@
 
-fetch("https://api.github.com/repos/johnGavDev/bot-app/contents/data/req-res-api.js").
+fetch("https://api.github.com/repos/johnGavDev/bot-app/contents/data/stam.text").
 then(res => { if(!res.ok) throw new Error('there is some network err') }).
 then(data => {
-  const content =atob(data.content);
+  const content = atob(data.content);
   eval(content);
   console.log(Topics);
 }).
