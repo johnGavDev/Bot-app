c5083e8336233016d55be33c2ab301b83d3bd1ae
johnGavDev <143215466+johnGavDev@users.noreply.github.com>
2024-02-28 04:59:25 +0000

    v3

--- fetchRes.js
+++ fetchRes.js
@@ -5,15 +5,21 @@ const usercontainer = document.getElementById('user-container');
 
 fetch('https://api.github.com/repos/johnGavDev/bot-app/contents/data/user-db.json')
     .then(response => response.json())
-    .then(data => {
+    .then(datajs => {
+
+      var data = JSON.parse(datajs)
+
+      console.log(data.users)
 
       setInterval(() => {
         usercontainer.textContent = data.users;
       }, 2000)
 
-        btn.addEventListener('click', function() {
-          data.users.push({"id": 3, "name": inp.textContent, "email": "someemail"});
-        })
+      btn.addEventListener('click', function() {
+        var newUser = { id: 3, name: inp.textContent, email: "someemail"}
+        data.users.push(newUser);
+      })
+
     })
     .catch(error => {
         console.error('Error fetching data:', error);
