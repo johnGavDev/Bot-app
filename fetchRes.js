ab4d34970e1b76f78cd440cb05b204c2d187fd30
johnGavDev <143215466+johnGavDev@users.noreply.github.com>
2024-02-28 04:39:57 +0000

    v3

--- fetchRes.js
+++ fetchRes.js
@@ -1,12 +1,20 @@
 
-fetch("https://api.github.com/repos/johnGavDev/bot-app/contents/data/stam.text").
-then(res => { if(!res.ok) {
-    throw new Error('there is some network err')
-  }
-    return res.json();
-}).
-then(data => {
-  const content = atob(data.content);
-  console.log(content + " and the topic is: " + Topics);
-}).
-catch(err => {console.error("err", err)});
+const btn = document.getElementById('sub-btn');
+const inp = document.getElementById('inp');
+const usercontainer = document.getElementById('user-container');
+
+fetch('users.json')
+    .then(response => response.json())
+    .then(data => {
+
+      setInterval(() => {
+        usercontainer.textContent = data.users;
+      }, 2000)
+
+        btn.addEventListener('click', function() {
+          data.users.push({"id": 3, "name": inp.textContent, "email": "someemail"});
+        })
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
